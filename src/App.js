3209b84467d098d5407a25f0333e265145f61fcc
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-17 11:42:34 +0300

    Add Balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import Balance from './components/Balance';
+
+import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </>
   );
 }
